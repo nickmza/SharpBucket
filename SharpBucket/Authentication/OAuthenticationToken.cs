a758efe67a68192a74c041b017b3b4b6ee588fec
Nick McKenzie <nickm@nvisionit.co.za>
2018-02-09 08:34:23 +0200

    Add support for OAuth Tokens

---
+++ SharpBucket/Authentication/OAuthenticationToken.cs
@@ -0,0 +1,34 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpBucket.Authentication
+{
+    /// <summary>
+    /// Allows SharpBucket to authenticate using a token obtained from the OAUTH https://bitbucket.org/site/oauth2/authorize endpoint. 
+    /// </summary>
+    public class OAuthenticationToken : Authenticate
+    {
+        private const string TokenType = "Bearer";
+        private string _token;
+        private string _baseUrl;
+
+        public OAuthenticationToken(string token, string baseUrl)
+        {
+            _token = token;
+            _baseUrl = baseUrl;
+        }
+
+        public void GetToken()
+        {
+            client = new RestClient(_baseUrl)
+            {
+                Authenticator = new OAuth2AuthorizationRequestHeaderAuthenticator(_token, TokenType)
+            };
+        }
+    }
+}
