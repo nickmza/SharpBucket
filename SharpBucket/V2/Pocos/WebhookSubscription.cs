96d9679ba2a4541e34d20f04e077ac6245acc90a
Johan Jordaan <JohanJ@nvisionit.co.za>
2018-07-02 12:45:38 +0200

    Add Pocos classes for Webhook Subcriptions Http/Get

---
+++ SharpBucket/V2/Pocos/WebhookSubscription.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpBucket.V2.Pocos
+{
+    public class WebhookSubscription
+    {
+        public string uuid { get; set; }
+        public string url { get; set; }
+        public string description { get; set; }
+        public string subject_type { get; set; }
+        public string subject { get; set; }
+        public bool active { get; set; }
+        public string create_at { get; set; }
+        public IList<string> events { get; set; }
+    }
+}
---
+++ SharpBucket/V2/Pocos/WebhookSubscriptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpBucket.V2.Pocos
+{
+    public class WebhookSubscriptions: IteratorBasedPage<WebhookSubscription>
+    {
+
+    }
+}
